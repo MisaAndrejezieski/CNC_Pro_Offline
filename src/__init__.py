2ff9e7c9b3788575d552f144e2bd32ed48fa9e68
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-05-05 18:00:51 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
-"""Módulo GUI do CNC Pro"""
+"""
+CNC Pro - Gerador de G-code Profissional
+Versão Desktop Offline - Sem Limitações
+"""
 
-from .main_window import MainWindow
-
-__all__ = ['MainWindow']
+__version__ = "3.0.0"
+__author__ = "CNC Pro"
+__license__ = "MIT"
+__description__ = "Gerador profissional de G-code a partir de imagens"
